652ca4d498a1fef18ff7067c0af95b1334eef400
Marek Lapinski <mrk.lapinski@gmail.com>
2016-08-10 10:46:19 +0200

    Commit przed deployem

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -12,4 +12,6 @@ urlpatterns = patterns('',
                        url(r'^restricted/', views.restricted, name='restricted'),
                        # url(r'^logout/$', views.user_logout, name='logout'),
                        url(r'^homepage/$', views.index, name='homepage'),
+                       url(r'^like_category/$', views.like_category, name='like_category'),
+                       url(r'^suggest_category/$', views.suggest_category, name='suggest_category'),
                        )
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -173,3 +173,45 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect('/rango/')
+
+@login_required
+def like_category(request):
+
+    cat_id = None
+    if request.method == 'GET':
+        cat_id = request.GET['category_id']
+
+    likes = 0
+    if cat_id:
+        cat = Category.objects.get(id=int(cat_id))
+        if cat:
+            likes = cat.likes + 1
+            cat.likes = likes
+            cat.save()
+
+    return HttpResponse(likes)
+
+
+def get_category_list(max_results=0, starts_with=''):
+    cat_list = []
+    if starts_with:
+        cat_list = Category.objects.filter(name__istartswith=starts_with)
+
+    if max_results > 0:
+        if cat_list.count() > max_results:
+            cat_list = cat_list[:max_results]
+
+    return cat_list
+
+def suggest_category(request):
+
+        cat_list = []
+        starts_with = ''
+        if request.method == 'GET':
+                starts_with = request.GET['suggestion']
+                print(starts_with)
+
+        cat_list = get_category_list(8, starts_with)
+        print(cat_list)
+
+        return render(request, 'rango/category_list.html', {'cat_list': cat_list})
